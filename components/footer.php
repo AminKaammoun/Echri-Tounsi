e174484b2e6ca5d870c5527bb18a9c9464237505
Abir <abiraloulou5@gmail.com>
2024-04-19 22:14:42 +0100

    footer update

--- components/footer.php
+++ components/footer.php
@@ -4,40 +4,32 @@
 
       <div class="box">
          <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
-         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
       </div>
 
       <div class="box">
          <h3>extra links</h3>
-         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
-         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
-         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
-         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
       </div>
 
       <div class="box">
          <h3>contact us</h3>
-         <a href="tel:1234567890"><i class="fas fa-phone"></i> +216 12 345 678</a>
-         <a href="tel:11122233333"><i class="fas fa-phone"></i> +216 87 654 321</a>
-         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> EchriTounsi@gmail.com</a>
-         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> sfax, Tunisia - 3021 </a>
+         <a href="tel:74747474"><i class="fas fa-phone"></i> +216 74 747 474</a>
+         <a href="mailto:echritounsi@gmail.com"><i class="fas fa-envelope"></i> echritounsi@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Sfax, Tunisia - 3021 </a>
       </div>
 
-      <div class="box">
-         <h3>follow us</h3>
-         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
-         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
-         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
-         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
-      </div>
 
    </section>
 
    <div class="credit">&copy; copyright @
-      <?= date('Y'); ?> by <span>Echri Tounsi</span> | all rights reserved!
+      <?= date('Y'); ?> by <span>Echri Tounsi</span> | all rights reserved.
    </div>
 
 </footer>
