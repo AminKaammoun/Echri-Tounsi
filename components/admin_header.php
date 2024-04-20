d2d757412b2fd35c38013679d9672dd733762397
Abir <abiraloulou5@gmail.com>
2024-04-20 23:00:48 +0100

    alert msg admin update

--- components/admin_header.php
+++ components/admin_header.php
@@ -1,38 +1,56 @@
 <?php
-   if(isset($message)){
-      foreach($message as $message){
+if (isset($message)) {
+   foreach ($message as $message) {
+      if (
+         $message === "product name already exist!"
+         || $message === "category name already exist!"
+         || $message === "Sub category name already exist!"
+         || $message === "please enter old password!"
+         || $message === "incorrect username or password!"
+      ) {
+         echo '
+         <div class="message-warn">
+            <span>' . $message . '</span>
+            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+         </div>
+         ';
+      } else {
          echo '
          <div class="message">
-            <span>'.$message.'</span>
+            <span>' . $message . '</span>
             <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
          </div>
          ';
       }
+
+
    }
+}
 ?>
 
 <header class="header">
 
    <section class="flex">
-       
-      
-   <?php
-   $current_page = basename($_SERVER['PHP_SELF']);
-   if ($current_page !== 'dashboard.php') {
-      echo '<a href="javascript:void(0);" onclick="goBack();" class="back-button navbar">
+
+
+      <?php
+      $current_page = basename($_SERVER['PHP_SELF']);
+      if ($current_page !== 'dashboard.php') {
+         echo '<a href="javascript:void(0);" onclick="goBack();" class="back-button navbar">
                 <i class="fas fa-arrow-left"></i> Back
             </a>';
-  }
-   ?>
-    <?php
-        if ($current_page !== 'dashboard.php') {
-            echo '<a href="javascript:void(0);" onclick="goForward();" class="forward-button navbar">
+      }
+      ?>
+      <?php
+      if ($current_page !== 'dashboard.php') {
+         echo '<a href="javascript:void(0);" onclick="goForward();" class="forward-button navbar">
                     <i class="fas fa-arrow-right"></i> Forward
                   </a>';
-        }
-        ?>
-      <a href="../admin/dashboard.php" class="logo " style="display: flex;justify-content: center;align-items: center;gap:2rem"><img
-            src="../images/logo_board.png" width="150px" />
+      }
+      ?>
+      <a href="../admin/dashboard.php" class="logo "
+         style="display: flex;justify-content: center;align-items: center;gap:2rem"><img src="../images/logo_board.png"
+            width="150px" />
 
       </a>
       <nav class="navbar">
@@ -53,9 +71,9 @@
 
       <div class="profile">
          <?php
-            $select_profile = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
-            $select_profile->execute([$admin_id]);
-            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         $select_profile = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
+         $select_profile->execute([$admin_id]);
+         $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
          ?>
          <p><?= $fetch_profile['name']; ?></p>
          <a href="../admin/update_profile.php" class="btn">update profile</a>
@@ -63,43 +81,44 @@
             <a href="../admin/register_admin.php" class="option-btn">register</a>
             <a href="../admin/admin_login.php" class="option-btn">login</a>
          </div>
-         <a href="../components/admin_logout.php" class="delete-btn" onclick="return confirm('logout from the website?');">logout</a> 
+         <a href="../components/admin_logout.php" class="delete-btn"
+            onclick="return confirm('logout from the website?');">logout</a>
       </div>
       <div class="navbar">
          <p style="font-size: 20px;"><?= $fetch_profile['name']; ?></p>
       </div>
-      
+
    </section>
    <script>
       function goBack() {
-        if (window.location.pathname !== 'http://localhost/Echri-Tounsi/home.php') {
+         if (window.location.pathname !== 'http://localhost/Echri-Tounsi/home.php') {
             history.back();
-        } else {
+         } else {
             window.location.href = 'other_page.php'; // Change 'other_page.php' to the desired fallback page
-        }
-    }
-      window.onscroll = function() {scrollFunction()};
+         }
+      }
+      window.onscroll = function () { scrollFunction() };
       function goForward() {
-            history.forward(); // Using the browser's forward function
-        }
+         history.forward(); // Using the browser's forward function
+      }
 
-function scrollFunction() {
-   var backButton = document.querySelector('.back-button');
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      backButton.style.bottom = "10px"; // Adjust this value as needed
-   } else {
-      backButton.style.bottom = "20px"; // Adjust this value as needed
-   }
-   var forwardButton = document.querySelector('.forward-button');
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      forwardButton.style.bottom = "10px"; // Adjust this value as needed
-   } else {
-      forwardButton.style.bottom = "20px"; // Adjust this value as needed
-   }
-}
-window.onload = function() {
-        scrollFunction();
-    };
-</script>
-   
+      function scrollFunction() {
+         var backButton = document.querySelector('.back-button');
+         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backButton.style.bottom = "10px"; // Adjust this value as needed
+         } else {
+            backButton.style.bottom = "20px"; // Adjust this value as needed
+         }
+         var forwardButton = document.querySelector('.forward-button');
+         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            forwardButton.style.bottom = "10px"; // Adjust this value as needed
+         } else {
+            forwardButton.style.bottom = "20px"; // Adjust this value as needed
+         }
+      }
+      window.onload = function () {
+         scrollFunction();
+      };
+   </script>
+
 </header>
